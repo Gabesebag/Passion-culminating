cf0cde6b5109882c1853aa2ade10334670bea53d
Gabriel <gabriel.theodore352@gmail.com>
2026-06-10 12:36:26 -0400

    .

--- Variables.js
+++ Variables.js
@@ -6,5 +6,15 @@ let nameInputFocused = false; // Track if the name input box is focused
 let startscreen = 0
 let Arena = 1
 
+let player1X = 0
+let player1Y = 0
+let player2X = 0
+let player2Y = 0
+
+let blueplayerx = 0
+let blueplayery = 0
+let redplayerx = 0
+let redplayery = 0
+
 let player1name = ""
 let player2name = ""
